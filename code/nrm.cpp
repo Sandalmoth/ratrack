f96c09b682b6df26a9c640b8270db7ef74adc8f6
Jonathan Lindström <jonathan.lindstrom@lnu.se>
2019-10-24 16:04:52 +0200

    updated stochastic simulator with more useful interface (breaks overall function for now)

--- code/nrm.cpp
+++ code/nrm.cpp
@@ -14,67 +14,44 @@
 using namespace std;
 
 
-const string VERSION = "0.0.1";
+const string VERSION = "0.1.0";
 
 
 struct Arguments {
   int n0;
   vector<double> birth_rate;
-  double death_rate;
-  double interaction_birth_rate;
   double interaction_death_rate;
-  double t_end;
+  vector<double> times;
 };
 
 
 class Cell {
 public:
-  Cell(vector<double> b2, double d, double p, double q, double t_max)
-    : b_base(b2)
-    , d(d)
-    , p(p)
+  Cell(vector<double> br, double q, float t_end)
+    : birth_rates(br)
     , q(q)
-    , t_max(t_max) {
+    , t_end(t_end) {
 
-
-    // TODO Switch to monotonic cubic interpolation
-    // cout << "CELL CONSTRUCTOR" << endl;
-
-
-    // cout << "base vector " << b.size() << " \n";
-    // for (auto x: b_base) cout << x << endl;
-
-    // std::cout.precision(3);
-
-    for (int z = 0; z < t_max * INTERPOLATIONS; ++z) {
-      size_t i = static_cast<double>(z) / static_cast<double>(INTERPOLATIONS) / t_max * (b_base.size() - 1);
-      double x = static_cast<double>(z) / static_cast<double>(INTERPOLATIONS) / t_max * (b_base.size() - 1) - i;
-      b.push_back(b_base[i] * (1 - x) + b_base[i+1] * x);
-      // cout << z << '\t' << i << '\t' << x << '\t' << b.back() << endl;
-    }
-    b.push_back(b_base.back());
-
-    // cout << "interpolation vector " << b.size() << " \n";
-    // for (auto x: b) cout << x << endl;
-    // cout << "CELL CONSTRUCTOR END" << endl;
+    dt = t_end / (birth_rates.size() - 1);
   }
 
+  // interpolate between a and b at coordinate x = [0, 1]
+  double interpolate(double a, double b, double x) {
+    return a*(1.0 - x) + b*x;
+  }
 
-  // const Cell &operator=(const Cell &other) {
-  // }
 
   double get_birth_rate(double t) {
-    // cout << endl << static_cast<size_t>(t*INTERPOLATIONS) << '\t' << b.size() << endl;
-    return b[static_cast<size_t>(t*INTERPOLATIONS)];
+    if (t > t_end)
+      return birth_rates.back();
+    size_t i = t / dt;
+    return interpolate(birth_rates[i], birth_rates[i+1], (t - i*dt)/dt);
   }
 
-  const int INTERPOLATIONS = 100.0;
-  vector<double> b_base;
-  vector<double> b;
-  double d;
-  double p;
+  vector<double> birth_rates;
+  double dt;
   double q;
-  double t_max;
+  double t_end;
 };
 
 
@@ -105,33 +82,43 @@ public:
     X[0] = count;
   }
 
+
+  auto get_cell_count() {
+    return X[0];
+  }
+
+  auto get_time() {
+    return t;
+  }
+
+  auto get_birth_rate(size_t cell_type) {
+    return a[cell_type*2];
+  }
+
+
+  auto get_death_rate(size_t cell_type) {
+    return a[cell_type*2 + 1];
+  }
+
+
+
   void get_birth_rates() {
     for (size_t i = 0; i < type_count; ++i) {
       double rate = cells[i].get_birth_rate(t);
-      double interaction = cells[i].p * (cells[i].get_birth_rate(t) - cells[i].d); // Constant carrying capacity
-      // double interaction = cells[i].p; // Carrying capacity varies with effective reproduction rate
-      int sizemult = X.sum() - 1;
-      double ai = X[i] * rate - sizemult * X[i] * interaction;
-      if (ai >= 0.0)
-        a[i*2] = ai;
-      else {
-        a[i*2] = 0;
-        a[i*2 + 1] -= ai;
-      }
+      double ai = X[i] * rate;
+      a[i*2] = ai;
     }
   }
   void get_death_rates() {
     for (size_t i = 0; i < type_count; ++i) {
-      double rate = cells[i].d;
-      double interaction = cells[i].q * (cells[i].get_birth_rate(t) - cells[i].d); // Constant carrying capacity
-      // double interaction = cells[i].q; // Carrying capacity varies with effective reproduction rate
+      double interaction = cells[i].q * cells[i].get_birth_rate(t); // Constant carrying capacity
       int sizemult = X.sum() - 1;
-      a[i*2 + 1] = X[i] * rate + sizemult * X[i] * interaction;
+      a[i*2 + 1] = sizemult * X[i] * interaction;
     }
   }
 
   void update_rates() {
-    get_death_rates(); // order is important!
+    get_death_rates(); // order is not important now (I think)!
     get_birth_rates();
   }
 
@@ -148,28 +135,11 @@ public:
 
   void simulate(double interval) {
     double t_end = t + interval;
-    init();
-
-    // std::cout.precision(3);
-    std::cout << "time\tsize\trate\n";
-    std::cout << t;
-    for (auto x: X) std::cout << '\t' << x;
-    std::cout << '\t' << cells[0].get_birth_rate(t) << '\n';
-
-    double print_interval = 0.1;
-    double next_print = 0.0 + print_interval;
-
+    if (t == 0.0)
+      init();
 
     while (t < t_end) {
       dt = (P - T) / a;
-      // double d = dt.min();
-
-      if (t > next_print) {
-        std::cout << t;
-        for (auto x: X) std::cout << '\t' << x;
-        std::cout << '\t' << cells[0].get_birth_rate(t) << '\n';
-        next_print += print_interval;
-      }
 
       size_t u = std::min_element(std::begin(dt), std::end(dt)) - std::begin(dt);
       double d = dt[u];
@@ -187,16 +157,10 @@ public:
       update_rates();
 
     }
-    // also print end state
-    std::cout << t;
-    for (auto x: X) std::cout << '\t' << x;
-    std::cout << '\t' << cells[0].get_birth_rate(t) << '\n';
-    next_print += print_interval;
-
   }
 
 private:
-  double t = 0;
+  double t = 0.0;
   size_t type_count;
   std::valarray<double> X;
   std::valarray<double> a;
@@ -222,35 +186,44 @@ int main(int argc, char **argv) {
     TCLAP::CmdLine cmd("General treatment simulator", ' ', VERSION);
 
     TCLAP::ValueArg<int> a_n0("n", "n0", "Starting cell count", true, 100, "integer", cmd);
-    TCLAP::ValueArg<string> a_birth_rate("b", "birth-rate", "Birth rate", true, "", "[0, 1, 2]", cmd);
-    TCLAP::ValueArg<double> a_death_rate("d", "death_rate", "Death rate", true, 100, "double", cmd);
-    TCLAP::ValueArg<double> a_interaction_birth_rate("p", "interaction-birth-rate", "Interaction Birth rate", true, 100, "double", cmd);
+    TCLAP::ValueArg<string> a_birth_rate("b", "birth-rate", "Birth rate", true, "", "[0, 1, 2, ...]", cmd);
     TCLAP::ValueArg<double> a_interaction_death_rate("q", "interaction-death_rate", "Interaction Death rate", true, 100, "double", cmd);
-    TCLAP::ValueArg<double> a_t("t", "t-max", "Simulation time", true, 100, "double", cmd);
+    TCLAP::ValueArg<string> a_times("t", "measure-times", "Times to measure population size", true, "", "[0, 1, 2, ...]", cmd);
 
     cmd.parse(argc, argv);
 
     a.n0 = a_n0.getValue();
     string bstring = a_birth_rate.getValue();
     smatch m_b;
-    regex re_b("\\d+\\.\\d+");
-    while (regex_search(bstring, m_b, re_b)) {
+    regex re_fp("\\d+\\.\\d+");
+    while (regex_search(bstring, m_b, re_fp)) {
       for (auto x: m_b) {
         a.birth_rate.push_back(stod(x));
       }
       bstring = m_b.suffix().str();
     }
-    a.death_rate = a_death_rate.getValue();
-    a.interaction_birth_rate = a_interaction_birth_rate.getValue();
     a.interaction_death_rate = a_interaction_death_rate.getValue();
-    a.t_end = a_t.getValue();
+    string tstring = a_times.getValue();
+    smatch m_t;
+    while (regex_search(tstring, m_t, re_fp)) {
+      for (auto x: m_t) {
+        a.times.push_back(stod(x));
+      }
+      tstring = m_t.suffix().str();
+    }
 
     // Sanity checks
     assert(a.n0 > 0);
-    assert(a.death_rate >= 0);
-    assert(a.interaction_birth_rate >= 0);
-    assert(a.interaction_death_rate >= 0);
-    assert(a.t_end > 0);
+    assert(a.interaction_death_rate >= 0.0);
+    for (auto rate: a.birth_rate) {
+      assert(rate >= 0.0);
+    }
+    for (size_t i = 1; i < a.times.size(); ++i) {
+      assert(a.times[i-1] >= 0.0);
+      // verify that they are sorted
+      assert(a.times[i-1] < a.times[i]);
+    }
+
 
   } catch (TCLAP::ArgException &e) {
     cerr << "TCLAP Error: " << e.error() << endl << "\targ: " << e.argId() << endl;
@@ -259,10 +232,15 @@ int main(int argc, char **argv) {
 
   // ### Simulation ### //
 
-  Cell wt(a.birth_rate, a.death_rate, a.interaction_birth_rate, a.interaction_death_rate, a.t_end);
-  // LB<Cell> lb(Cell(a.birth_rate, a.death_rate, a.interaction_birth_rate, a.interaction_death_rate, a.t_end));
+  Cell wt(a.birth_rate, a.interaction_death_rate, a.times.back());
   LB<Cell> lb(wt);
   lb.set_cell_count(a.n0);
-  lb.simulate(a.t_end);
+  std::cout << "time\tsize\trate\n";
+  double t_prev = 0.0;
+  for (auto time: a.times) {
+    lb.simulate(time - t_prev);
+    t_prev = time;
+    std::cout << time << '\t' << lb.get_cell_count() << '\t' << wt.get_birth_rate(time) << '\n';
+  }
 }
 
